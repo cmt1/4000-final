a88c90f0e950f4e14437ee4d3b38b1873223730f
cmt1 <cmt1@protonmail.com>
2018-06-17 22:29:09 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# 4000-final
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# Modify App
+
+Template <template>
+Style <style>
+Logic <script>
+    
+# Debug
+
+Install Vue dev tools for chrome
+
+# Deploy App
+
+Create repo on Github 
+
+git add -A , to add all files
+
+Configure Webpack
+
+in the config/index.hs change parths from dist to docs and assestsPublicPath to ''
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+commit and push changes
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+      <ul>
+          <li><router-link to="/">Sunset</router-link></li>
+             <li><router-link to="Sunrise">Sunrise</router-link></li>
+    </ul> 
+      
+    <h1>{{ msg }}</h1>
+       <form v-on:submit.prevent="sunTimes">
+      <p>Latitude <input type="text" v-model="rhyme"> Longitude <input type="text" v-model="phrase"> <button type="submit">Search</button></p>
+    </form>
+     
+   
+    <h2>API Credit</h2>
+      
+    <ul>
+      <li>
+        <a href="https://sunrise-sunset.org/api"
+          target="_blank"
+        >
+          Sunset and sunrise times API
+        </a>
+      </li>
+      
+    </ul>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                msg: 'Sunset Times'
+            }
+        },
+    methods: {
+  findWords: function(){
+    axios.get('https://api.sunrise-sunset.org/json', {
+      params: {
+        lt: this.lat,
+        lg: this.long
+      }
+    })
+    .then(response => {
+      this.results = response.data;
+    })
+    .catch(error => {
+      this.errors.push(error);
+    });
+  }
+}
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1,
+    h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+</style>
